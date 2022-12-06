f62e0fb5baaf8a667b2f163335259783db3b54b3
AmalAlby <amalalbyn@gmail.com>
2022-12-06 22:10:04 +0530

    Patient backend created

---
+++ src/test/java/com/nest/patientappbackend/PatientappbackendApplicationTests.java
@@ -0,0 +1,13 @@
+package com.nest.patientappbackend;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PatientappbackendApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
